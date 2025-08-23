508d1b854604da565e2ebbdca2fe7fbc8c6130c4
panshulthinks <panshularora2@gmail.com>
2025-08-23 20:38:45 +0530

    Create lec2.java

---
+++ lec2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class lec2 {
+    public static void main(String[] args){
+        String name = "Panshul Arora";
+        int age = 18;
+        double salary = 2500;
+
+//output
+        int a = 25;
+        int b = 10;
+        int diff = b-a;
+        System.out.println(diff);
+
+//input
+        Scanner sc = new Scanner(System.in);
+        String naame = sc.next();
+        System.out.println(naame);
+
+    }
+}
