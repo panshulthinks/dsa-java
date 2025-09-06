bf5dfd7ae3692bfd4a05882f04da8acb2936dd5f
panshulthinks <panshularora2@gmail.com>
2025-09-06 13:04:59 +0530

    Create ceiling.java

---
+++ binarysearch/ceiling.java
@@ -0,0 +1,40 @@
+package binarysearch;
+
+public class ceiling {
+    public static void main(String[] args) {
+
+        int[] arr = {-18, -3, 1, 3, 5, 7};
+        int target = 89;
+        int ans = ceiling(arr, target);
+        System.err.println(ans);
+        
+    }
+
+        static int ceiling(int[] arr, int target) {
+
+        // but what if the target is greater than the greatest number in the array
+        if (target > arr[arr.length - 1]) {
+            return -1;
+        }
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+        return start;
+    }
+}
+
+
