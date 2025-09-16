89ac48559fbe68010cd03c9f1c57907d83d4c75c
panshulthinks <panshularora2@gmail.com>
2025-09-16 23:28:56 +0530

    Create question_1346.java

---
+++ leetcodeques/array/question_1346.java
@@ -0,0 +1,15 @@
+package array;
+// https://leetcode.com/problems/check-if-n-and-its-double-exist/
+
+public class question_1346 {
+    public boolean checkIfExist(int[] arr) {
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                if(i != j && arr[i] == 2* arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
