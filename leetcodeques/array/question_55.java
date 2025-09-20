3401ec4e8cf62614a95c0f9158b56e36c44ae096
panshulthinks <panshularora2@gmail.com>
2025-09-20 16:51:32 +0530

    Create question_55.java

---
+++ leetcodeques/array/question_55.java
@@ -0,0 +1,28 @@
+c1package leetcodeques;
+import java.util.*;
+// https://leetcode.com/problems/jump-game/
+
+public class question_55 {
+    public boolean canJump(int[] nums) {
+        int steps = nums[0]; // how far we can go initially
+
+        for (int i = 1; i < nums.length; i++) {
+            steps--; // moving forward uses 1 step
+
+            if (steps < 0) {
+                return false; // stuck before reaching this index
+            }
+
+            // refill steps if nums[i] allows a farther jump
+            steps = Math.max(steps, nums[i]);
+        }
+
+        return true; // reached the end
+    }
+    public static void main(String[] args){
+        question_55 answer = new question_55();
+
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println("RESULT " + answer.canJump(nums));
+    }
+}
