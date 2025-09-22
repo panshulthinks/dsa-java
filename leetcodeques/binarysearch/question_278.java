b3e8d20a8d2a8886b3b1729fb54b94f601261ad8
panshulthinks <panshularora2@gmail.com>
2025-09-22 16:28:21 +0530

    Create question_278.java

---
+++ leetcodeques/binarysearch/question_278.java
@@ -0,0 +1,22 @@
+package binarysearch;
+// https://leetcode.com/problems/first-bad-version/description/
+public class question_278 {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (isBadVersion(mid)) {
+                // mid might be the first bad version,
+                // but check if there is an earlier one
+                right = mid;
+            } else {
+                // mid is good, so the first bad must be after mid
+                left = mid + 1;
+            }
+        }
+        
+        return left; // or right (both are same here)
+    }
+}
