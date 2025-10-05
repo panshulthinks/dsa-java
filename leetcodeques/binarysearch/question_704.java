84c0452073d71b8d277a76179854f40874113bcb
panshulthinks <panshularora2@gmail.com>
2025-10-05 16:34:41 +0530

    Create question_704.java

---
+++ leetcodeques/binarysearch/question_704.java
@@ -0,0 +1,22 @@
+package binarysearch;
+// https://leetcode.com/problems/binary-search/
+public class question_704 {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2; 
+            
+            if (nums[mid] == target) {
+                return mid; 
+            } else if (nums[mid] < target) {
+                start = mid + 1; 
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+}
